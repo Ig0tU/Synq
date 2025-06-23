a97e3ee98194bfc4ba7a5722a7d61fd2ee912fae
dev patel <WebashalarForML@users.noreply.huggingface.co>
2025-06-23 08:37:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -151,8 +151,8 @@ def infer():
                 pads=[0, 10, 0, 0],
                 crop=[0, -1, 0, -1],
                 box=[-1, -1, -1, -1],
-                face_det_batch_size=4,
-                wav2lip_batch_size=32,
+                face_det_batch_size=8,
+                wav2lip_batch_size=64,
                 #face_det_batch_size=16,
                 #wav2lip_batch_size=128,
                 img_size=96
